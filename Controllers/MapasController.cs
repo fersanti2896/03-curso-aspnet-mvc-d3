113416372c9b648e6208a2b76aceda9962acdff3
Fernando Nicolás <fersanti2896@gmail.com>
2022-08-05 10:08:35 -0500

    Creando grafica totales en ERPastel.cshtml

--- Controllers/MapasController.cs
+++ Controllers/MapasController.cs
@@ -3,8 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Pedidos.Controllers {
     public class MapasController : Controller {
-        
         public ActionResult EstadosRepublicas() {
+            return View();
+        }
+
+        public ActionResult ERPastel() {
             return View();
         }
     }
